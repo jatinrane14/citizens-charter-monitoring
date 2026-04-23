f2750b739abd693806d4efdca4264fbf6892e8d0
jatinrane14 <jatinrane23@gmail.com>
2026-04-23 23:04:11 +0530

    Implement postal and citizen model

--- Backend/src/main/java/com/minor/charter/monitoring/models/CitizenModel.java
+++ Backend/src/main/java/com/minor/charter/monitoring/models/CitizenModel.java
@@ -1,4 +1,60 @@
 package com.minor.charter.monitoring.models;
 
 public class CitizenModel extends UserBaseModel{
+    private String address;
+    private String city;
+    private String state;
+    private String pincode;
+
+    private String aadhaarNumber; // optional
+
+    private int totalServicesUsed;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTotalServicesUsed() {
+        return totalServicesUsed;
+    }
+
+    public void setTotalServicesUsed(int totalServicesUsed) {
+        this.totalServicesUsed = totalServicesUsed;
+    }
+
+    public String getAadhaarNumber() {
+        return aadhaarNumber;
+    }
+
+    public void setAadhaarNumber(String aadhaarNumber) {
+        this.aadhaarNumber = aadhaarNumber;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
--- Backend/src/main/java/com/minor/charter/monitoring/models/PostalStaffModel.java
+++ Backend/src/main/java/com/minor/charter/monitoring/models/PostalStaffModel.java
@@ -1,4 +1,69 @@
 package com.minor.charter.monitoring.models;
 
 public class PostalStaffModel extends UserBaseModel{
+    private String employeeId;
+    private String postOfficeName;
+    private String branchCode;
+    private String designation; // clerk, manager
+    private String workingArea;
+
+    private boolean isOnDuty;
+
+    private int yearsOfExperience;
+
+    public String getPostOfficeName() {
+        return postOfficeName;
+    }
+
+    public void setPostOfficeName(String postOfficeName) {
+        this.postOfficeName = postOfficeName;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getBranchCode() {
+        return branchCode;
+    }
+
+    public void setBranchCode(String branchCode) {
+        this.branchCode = branchCode;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getWorkingArea() {
+        return workingArea;
+    }
+
+    public void setWorkingArea(String workingArea) {
+        this.workingArea = workingArea;
+    }
+
+    public boolean isOnDuty() {
+        return isOnDuty;
+    }
+
+    public void setOnDuty(boolean onDuty) {
+        isOnDuty = onDuty;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
 }
