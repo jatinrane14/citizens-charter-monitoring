13bafe0590cc3633c83c72f0653251e285c00c4b
jatinrane14 <jatinrane23@gmail.com>
2026-05-03 12:19:21 +0530

    Implement Spring Security and Spring Data JPA for database connectivity

--- Backend/src/main/java/com/minor/charter/monitoring/Application.java
+++ Backend/src/main/java/com/minor/charter/monitoring/Application.java
@@ -1,13 +1,22 @@
 package com.minor.charter.monitoring;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+    @Value("${spring.datasource.url}")
+    private String url;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+
 	}
+    @PostConstruct
+    public void print() {
+        System.out.println("DB URL: " + url);
+    }
 
 }
